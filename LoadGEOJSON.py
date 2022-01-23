9043684890473ed374a388aae456fa4eab3590bc
Lukáš Nekola <nekola.luky@seznam.cz>
2022-01-23 10:01:31 +0100

    definition read

--- LoadGEOJSON.py
+++ LoadGEOJSON.py
@@ -5,3 +5,18 @@ class File:
         self.fileName=fileName
         self.dataJSON = {}
         self.newPolylines = []
+
+    def read(self):
+        try:
+            with open(self.fileName, "r", encoding="UTF-8") as loadFile:
+                self.dataJSON = json.load(loadFile)
+                return self.dataJSON["features"]
+        except PermissionError:
+            print(f"Nemáš umožněný přístup do souboru {self.fileName}")
+            exit()
+        except ValueError:
+            print(f"V souboru {self.fileName} je chyba")
+            exit()
+        except FileNotFoundError:
+            print(f"Soubor {self.fileName} nebyl nalezen")
+            exit()
