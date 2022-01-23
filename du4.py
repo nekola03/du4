036855394afb7f3b560c37a9ec2c9db4f4f8dba4
Lukáš Nekola <nekola.luky@seznam.cz>
2022-01-23 10:40:06 +0100

    compete parser

--- du4.py
+++ du4.py
@@ -15,3 +15,18 @@ def process(file, max_length):
             print("Chybí data")
             exit()
     return multilines
+
+parser = argparse.ArgumentParser(description="Zadej povinné parametry")
+parser.add_argument('-f', '--file', default=None, help="Zadej název vstupního souboru s příponou GEOJSON či JSON")
+parser.add_argument('-o', '--output', default=None, help="Zadej název výstupního souboru s příponou GEOJSON či JSON")
+parser.add_argument('-l', '--length', default=None, help="Zadej maximální vzdálenost segmentu")
+args = parser.parse_args()
+if args.file != None and args.length != None and args.output != None:
+    file = File(args.file)
+    data = file.read()
+    file.polylines = process(data, float(args.length))
+    file.name = args.output
+    file.write()
+else:
+    print("Chybně zadané parametry")
+    exit()
