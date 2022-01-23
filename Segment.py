ab057e9f2e6542a4ddc2521c9f0a0daa75a668a7
Lukáš Nekola <nekola.luky@seznam.cz>
2022-01-23 09:46:38 +0100

    distance of on segment

--- Segment.py
+++ Segment.py
@@ -21,15 +21,3 @@ class Line:
     def distance(self):
         self.length = sqrt((self.pointFirst.x - self.pointSecond.x)**2 + (self.pointFirst.y - self.pointSecond.y)**2)
         return self.length
-
-    def divide(self, max_length):
-        createPol = Polyline.Polyline()
-
-        if self.distance()>max_length:
-            self.calculatePointBetween()
-            createPol.segmentAdd(Line(self.pointFirst, self.PointBetween)) #importováno z Polyline
-            createPol.segmentAdd(Line(self.PointBetween, self.pointSecond)) #importováno z Polyline
-            return createPol.divide_long_segments(max_length) #importováno z Polyline
-
-        createPol.segmentAdd(self) #importováno z Polyline
-        return createPol
