c1b50f66edff5075416928343c16db6cf333327b
Lukáš Nekola <nekola.luky@seznam.cz>
2022-01-23 14:12:56 +0100

    add comments
    reapir mistakes

--- du4.py
+++ du4.py
@@ -1,14 +1,14 @@
 import argparse
-from LoadGEOJSON import File
-from Polyline import Polyline
+from LoadGEOJSON import loadGeoJSON
+from Polyline import polyline
 
 def process(file, max_length):
     multilines = []
     for readsData in file:
         try:
             if readsData["geometry"]["type"] == "LineString":
-                polyline = Polyline(readsData)
-                multilines.append(polyline.divide_long_segments(max_length))
+                polylin = polyline(readsData)
+                multilines.append(polylin.divide_long_segments(max_length))
             elif readsData["geometry"]["type"] != "LineString":
                 print("Chybná data:", readsData["properties"]["OBJECTID"])
         except KeyError:
@@ -21,11 +21,11 @@ parser.add_argument('-f', '--file', default=None, help="Zadej název vstupního 
 parser.add_argument('-o', '--output', default=None, help="Zadej název výstupního souboru s příponou GEOJSON či JSON")
 parser.add_argument('-l', '--length', default=None, help="Zadej maximální vzdálenost segmentu")
 args = parser.parse_args()
-if args.file != None and args.length != None and args.output != None:
-    file = File(args.file)
+if args.file is not None and args.length is not None and args.output is not None:
+    file = loadGeoJSON(args.file)
     data = file.read()
     file.polylines = process(data, float(args.length))
-    file.name = args.output
+    file.fileName = args.output
     file.write()
 else:
     print("Chybně zadané parametry")
