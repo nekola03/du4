83ef1efb83d7fd46b4d288b76a4b5f40e83ca506
Lukáš Nekola <nekola.luky@seznam.cz>
2022-01-23 10:21:06 +0100

    complete geoJSON

--- LoadGEOJSON.py
+++ LoadGEOJSON.py
@@ -1,36 +1,36 @@
 import json
 
 class File:
-    def __init__(self, fileName):
-        self.fileName=fileName
-        self.dataJSON = {}
-        self.newPolylines = []
+    def __init__(self, name):
+        self.name=name
+        self.polylines = []
+        self.json_data = {}
 
     def read(self):
         try:
-            with open(self.fileName, "r", encoding="UTF-8") as loadFile:
-                self.dataJSON = json.load(loadFile)
-                return self.dataJSON["features"]
-        except PermissionError:
-            print(f"Nemáš umožněný přístup do souboru {self.fileName}")
+            with open(self.name, "r", encoding="UTF-8") as physicFile:
+                self.json_data = json.load(physicFile)
+                return self.json_data["features"]
+        except FileNotFoundError: # zjistuje, zda existuje
+            print(f"CHYBA: Pozadovany soubor {self.name} neexistuje. Program skonci.")
             exit()
-        except ValueError:
-            print(f"V souboru {self.fileName} je chyba")
+        except PermissionError: # zjistuje pristup k souboru
+            print(f"CHYBA: Nemam pristup k {self.name}.Program skonci.")
             exit()
-        except FileNotFoundError:
-            print(f"Soubor {self.fileName} nebyl nalezen")
+        except ValueError as e: # validuje i pokud se jedna o validni JSON
+            print(f"CHYBA: Soubor {self.name} neni validni. Program skonci.\n", e)
             exit()
 
     def write(self):
         try:
-            with open(self.fileName, "w", encoding="UTF-8") as writefile:
-                parties = []
-                for polyline in self.newPolylines:
-                    parties.append(polyline.get_object_for_json())
-                self.dataJSON["features"] = parties
-                json.dump(self.dataJSON, writefile)
-        except PermissionError:
-            print(f"CHYBA: Nemam pristup k {self.fileName}. Program skonci.")
+            with open(self.name, "w", encoding="UTF-8") as physicFile:
+                features = []
+                for polyline in self.polylines:
+                    features.append(polyline.get_object_for_json())
+                self.json_data["features"] = features
+                json.dump(self.json_data, physicFile)
+        except PermissionError: # zjistuje pristup k souboru
+            print(f"CHYBA: Nemam pristup k {self.name}. Program skonci.")
             exit()
         except:
             print(f"CHYBA: Nemuzu ulozit soubor.")
